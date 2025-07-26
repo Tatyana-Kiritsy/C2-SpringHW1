7027bd1a0504f3f1cd8017ba11af7751e257a590
1295957 <kissaroo@mail.ru>
2025-07-26 23:28:54 +0300

    CW2-SpringHW3

--- skyshop/src/main/java/org/skypro/skyshop/controller/ShopController.java
+++ skyshop/src/main/java/org/skypro/skyshop/controller/ShopController.java
@@ -2,16 +2,16 @@ package org.skypro.skyshop.controller;
 
 import org.skypro.skyshop.model.article.Article;
 import org.skypro.skyshop.model.basket.UserBasket;
+import org.skypro.skyshop.model.exceptions.ShopError;
 import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.search.SearchResult;
 import org.skypro.skyshop.service.BasketService;
 import org.skypro.skyshop.service.SearchService;
 import org.skypro.skyshop.service.StorageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.UUID;
@@ -22,7 +22,8 @@ public class ShopController {
     private final SearchService searchService;
     private final BasketService basketService;
 
-    public ShopController(StorageService storageService, SearchService searchService, BasketService basketService) {
+
+    public ShopController(StorageService storageService, SearchService searchService, BasketService basketService, ShopError shopError) {
         this.storageService = storageService;
         this.searchService = searchService;
         this.basketService = basketService;
@@ -44,13 +45,25 @@ public class ShopController {
     }
 
     @GetMapping("/basket/{id}")
-    public String addProduct(@PathVariable("id") UUID id){
+    public String addProduct(@PathVariable("id") UUID id) {
         basketService.addProduct(id);
         return "Продукт успешно добавлен";
     }
 
     @GetMapping("/basket")
-    public UserBasket getUserBasket(){
+    public UserBasket getUserBasket() {
         return basketService.getUserBasket();
+    }
+
+    @ControllerAdvice
+    public static class ShopControllerAdvice {
+
+        @ExceptionHandler(NoSuchFieldException.class)
+        public ResponseEntity<ShopError> handleException(NoSuchFieldException e) {
+            ShopError shopError = new ShopError("NO_SUCH_PRODUCT_WITH_THIS_ID",
+                    "Продукт не найден!");
+            return new ResponseEntity<ShopError>(shopError, HttpStatus.NOT_FOUND);
+        }
+
     }
 }
